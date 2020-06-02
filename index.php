0072b206a08f13451859e06bfca204d17136094d
rowend36 <rowendduke36@gmail.com>
2020-06-02 20:17:11 +0100

    1st work

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-echo "hi"
+<?php
+$html = <<<HTML
+<!doctype html>
+<html>
+<head>
+<title>Task 2</title>
+</head>
+<body>
+<h5>Team Storm</h5>
+HTML;
+$results = array();
+define("PASS_TEXT", 
+	"/^Hello World, this is (\w+\s+)+with HNGi7 ID \d+ using [^\s]+ for stage 2 task$/");
+$files = scandir("./scripts");
+foreach($files as $i){
+	if($i == "." || $i=="..")continue;
+	$value = file_get_contents("./scripts/".$i);
+	$score = preg_match(PASS_TEXT,$value);
+	$results[$i] = $score?"pass":"fail";
+}
+if (array_key_exists("json",$_GET)){
+	$myJSON = json_encode($results);
+	header('Content-Type: application/json');
+	echo $myJSON;
+}
+else{
+	echo $html;
+	foreach($results as $name=>$score){
+	if($score){
+		echo "<br/>".$name." : PASS";
+	}
+	else{
+		echo "<br/>".$name." :FAIL";
+	}
+	}
+	echo "</body></html>";
+}
+
+?>
